3a480f7c0be3ae77a768cb748213e25728b8843c
EricFrancischett <ericmellof@gmail.com>
2022-05-24 10:27:30 -0300

    Return Button created

--- safesign_app/lib/theme/colors_app.dart
+++ safesign_app/lib/theme/colors_app.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class ColorsApp {
   static Color appDarkGrey = const Color(0xFF222831);
   static Color appGrey = const Color(0xFF393E46);
-  static Color appBlue = const Color(0xFF222831);
+  static Color appBlue = const Color(0xFF00ADB5);
   static Color appLightGrey = const Color(0xFFEEEEEE);
 }
