9677b19f70fc9b899b960a9d733c99aa3756d96c
Gabriel Jungles Fernandes <71150878+matheusmei@users.noreply.github.com>
2022-05-24 10:53:19 -0300

    create a main buttom

--- safesign_app/lib/main.dart
+++ safesign_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:safesign_app/widgets/return_button.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -47,8 +49,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: const <Widget>[
             ReturnButton()
+
+
+            
+
           ],
         ),
       ),
---
+++ safesign_app/lib/widgets/main_buttom.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:safesign_app/theme/colors_app.dart';
+import 'package:safesign_app/theme/fonts_app.dart';
+
+class MainButtom extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+
+  const MainButtom({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: BoxDecoration(
+        color: ColorsApp.appBlue,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          width: 350,
+          height: 50,
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: FontsApp.mainFontText24.copyWith(
+                fontWeight: FontWeight.w600, color: ColorsApp.appLightGrey),
+          ),
+        ),
+      ),
+    );
+  }
+}
