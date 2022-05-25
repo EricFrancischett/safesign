0d63f242113b07db55a9644b9171375ba2f34acd
EricFrancischett <ericmellof@gmail.com>
2022-05-25 15:18:38 -0300

    Fix Error Dialog

--- lib/features/login/view/widgets/custom_error_dialog.dart
+++ lib/features/login/view/widgets/custom_error_dialog.dart
@@ -19,30 +19,40 @@ class CustomErrorDialog extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(
             border: Border.all(color: ColorsApp.appBlue, width: 3),
-            borderRadius: const BorderRadius.all(Radius.circular(15),),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(15),
+            ),
             color: ColorsApp.appDarkGrey.withOpacity(0.5),
           ),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             // child: BackdropFilter(
             //   filter: ImageFilter.blur(sigmaX: 5,sigmaY: 5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    errorMessage,
-                    textAlign: TextAlign.center,
-                    style: FontsApp.mainFontText24.copyWith(color: ColorsApp.appLightGrey),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  MainButtom(text: "Ok!", onPressed: onPressed)
-                ],
-              ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  errorMessage,
+                  textAlign: TextAlign.center,
+                  style: FontsApp.mainFontText24
+                      .copyWith(color: ColorsApp.appLightGrey),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                MainButtom(
+                    onPressed: onPressed,
+                    child: Text(
+                      "Ok",
+                      style: FontsApp.mainFontText24.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: ColorsApp.appLightGrey),
+                    ))
+              ],
             ),
           ),
         ),
+      ),
       // ),
     );
   }
