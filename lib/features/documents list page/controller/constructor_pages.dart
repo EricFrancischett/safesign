1bb330458e2c5a484b95c8b03593ed54909befcb
matheusmei <71150878+matheusmei@users.noreply.github.com>
2022-05-26 23:03:06 -0300

    try to create constructor

--- lib/features/documents list page/controller/constructor_pages.dart
+++ lib/features/documents list page/controller/constructor_pages.dart
@@ -2,8 +2,9 @@ class ConstructorPages {
   String? titlePage;
   Status status;
 
-ConstructorPages.toSign({this.titlePage, this.status = Status.toSign})
-ConstructorPages.pending({this.titlePage, this.status = Status.pending})
-ConstructorPages.available({this.titlePage, this.status = Status.available})
+  ConstructorPages.toSign({this.titlePage, this.status = Status.toSign});
+  ConstructorPages.pending({this.titlePage, this.status = Status.pending});
+  ConstructorPages.available({this.titlePage, this.status = Status.available});
 }
-enum Status {toSign, pending, available}
+
+enum Status { toSign, pending, available }
