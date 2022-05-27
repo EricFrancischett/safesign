071f2d1269d9f753b6bcf8e9a5ebea4d39aae471
EricFrancischett <ericmellof@gmail.com>
2022-05-27 15:26:22 -0300

    UploadPage Created

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safesign_app/features/login/view/login_page.dart';
+import 'package:safesign_app/features/upload/view/upload_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: UploadPage(),
     );
   }
 }
--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:safesign_app/core/theme/colors_app.dart';
 import 'package:safesign_app/core/widgets/custom_appbar.dart';
 import 'package:safesign_app/features/home/widgets/custom_drawer.dart';
 import 'package:safesign_app/features/home/widgets/custom_tile.dart';
-
+import 'package:safesign_app/features/home/widgets/fabio.dart';
+import 'package:safesign_app/features/upload/view/upload_page.dart';
 import '../../../core/models/user_model.dart';
 import '../controller/home_controller.dart';
 
@@ -31,39 +31,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Fabio(onPressed: () async {
+        await Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => UploadPage(),
+          ),
+        );
+      }),
       backgroundColor: ColorsApp.appDarkGrey,
       endDrawer: CustomDrawer(
-
           firstName: widget.user.firstName!,
           lastName: widget.user.lastName!,
           id: "1988069"),
       appBar: const CustomAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Observer(builder: (_) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              CustomTile(
-                  number: _controller.documentstoSignLength,
-                  title: "Documents to sign"),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomTile(
-                  number: _controller.pendingDocumentsLength,
-                  title: "Pending Documents"),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomTile(
-                  number: _controller.availableDocumentsLength,
-                  title: "Available Documents"),
-            ],
-          );
-        }),
-
+        child: Observer(
+          builder: (_) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CustomTile(
+                    number: _controller.documentstoSignLength,
+                    title: "Documents to sign"),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomTile(
+                    number: _controller.pendingDocumentsLength,
+                    title: "Pending Documents"),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomTile(
+                    number: _controller.availableDocumentsLength,
+                    title: "Available Documents"),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
