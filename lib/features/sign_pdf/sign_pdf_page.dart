66373863da82195880a540e7fe483a25d5db902a
EricFrancischett <ericmellof@gmail.com>
2022-05-30 09:53:10 -0300

    fix/fixing package bug

--- lib/features/sign_pdf/sign_pdf_page.dart
+++ lib/features/sign_pdf/sign_pdf_page.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
 
 class SignPdfPage extends StatefulWidget {
   final File file;
